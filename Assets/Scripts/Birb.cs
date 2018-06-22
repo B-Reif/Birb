be4e55eb6deef0e658e47d1b94c3d069ac612425
Bruce <btreif@gmail.com>
2018-06-22 01:38:18 -0700

    add basic test setup and tweak API

--- Assets/Scripts/Birb.cs
+++ Assets/Scripts/Birb.cs
@@ -3,47 +3,92 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 
-public static class Birb {
+public static class Birb
+{
 
-	abstract public class Component<P> {
-		abstract public Element<T> Render<T>(T props);
-	}
+    abstract public class Component
+    {
+        abstract public Element Render(Dictionary<string, object> props);
+    }
 
-	public class Element<P> {
-		public Component<P> component;
-		public GameObject prefab;
-		public P props;
+    public class Element
+    {
+        public Component component;
+        public GameObject prefab;
+        public Dictionary<string, object> props;
 
-		public Element(Component<P> component, P props)
-		{
-			this.component = component;
-			this.props = props;
-		}
+        public Element(Component component, Dictionary<string, object> props)
+        {
+            this.component = component;
+            this.props = props;
+        }
 
-		public Element(GameObject prefab, P props)
-		{
-			this.prefab = prefab;
-			this.props = props;
-		}
-	}
+        public Element(GameObject prefab, Dictionary<string, object> props)
+        {
+            this.prefab = prefab;
+            this.props = props;
+        }
+    }
 
-	public static Element<P> CreateElement<P>(GameObject prefab, P props)
-	{
-		return new Element<P>(prefab, props);
-	}
+    public static Element CreateElement(GameObject prefab, Dictionary<string, object> props)
+    {
+        return new Element(prefab, props);
+    }
 
-	public static Element<P> CreateElement<P>(Component<P> component, P props)
-	{
-		return new Element<P>(component, props);
-	}
+    public static Element CreateElement(Component component, Dictionary<string, object> props)
+    {
+        return new Element(component, props);
+    }
 
-	class BirbRoot : MonoBehaviour {
-		public Element<object> element;
-	}
+    class BirbRoot : MonoBehaviour
+    {
+        public Element element;
+        public BirbRenderer birbRenderer;
 
-	public static void Render(GameObject host, Element<object> element)
-	{
-		BirbRoot root = host.AddComponent(typeof(BirbRoot)) as BirbRoot;
-		root.element = element;
-	}
+        public void Draw()
+        {
+            birbRenderer.Draw(element);
+        }
+    }
+
+    public class Props : Dictionary<string, object> { }
+
+    class PropsComponent : MonoBehaviour
+    {
+        public Dictionary<string, object> props;
+    }
+
+    class BirbRenderer : MonoBehaviour
+    {
+        public GameObject parent;
+        GameObject instance;
+
+        public void Draw(Element element)
+        {
+            this.instance = Instantiate(element.prefab);
+            if (this.parent != null)
+            {
+                this.instance.transform.SetParent(this.parent.transform);
+            }
+            instance.SetActive(true);
+            PropsComponent component = instance.AddComponent(
+                typeof(PropsComponent)
+            ) as PropsComponent;
+            component.props = element.props;
+        }
+    }
+
+    public static void Render(GameObject host, Element element)
+    {
+        BirbRoot root = host.AddComponent(typeof(BirbRoot)) as BirbRoot;
+        BirbRenderer birbRenderer = host.AddComponent(
+            typeof(BirbRenderer)
+        ) as BirbRenderer;
+        birbRenderer.parent = host;
+
+        root.element = element;
+        root.birbRenderer = birbRenderer;
+        root.Draw();
+    }
+
 }
---
+++ Assets/Scripts/Tester.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Tester : MonoBehaviour
+{
+    public GameObject buttonPrefab;
+    public GameObject uiRoot;
+    public string text;
+
+    // Use this for initialization
+    void Start()
+    {
+        Birb.Props props = new Birb.Props();
+        props.Add("text", this.text);
+        var button = Birb.CreateElement(buttonPrefab, props);
+        Birb.Render(uiRoot, button);
+    }
+}
