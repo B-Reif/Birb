004a5ebafd0fb806815ff026a998232bdccbe173
Bruce Reif <bruce.reif@peanutlabs.com>
2018-06-21 18:10:01 -0700

    initial commit

---
+++ Assets/Scripts/Birb.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public static class Birb {
+
+	abstract public class Component<P> {
+		abstract public Element<T> Render<T>(T props);
+	}
+
+	public class Element<P> {
+		public Component<P> component;
+		public GameObject prefab;
+		public P props;
+
+		public Element(Component<P> component, P props)
+		{
+			this.component = component;
+			this.props = props;
+		}
+
+		public Element(GameObject prefab, P props)
+		{
+			this.prefab = prefab;
+			this.props = props;
+		}
+	}
+
+	public static Element<P> CreateElement<P>(GameObject prefab, P props)
+	{
+		return new Element<P>(prefab, props);
+	}
+
+	public static Element<P> CreateElement<P>(Component<P> component, P props)
+	{
+		return new Element<P>(component, props);
+	}
+
+	class BirbRoot : MonoBehaviour {
+		public Element<object> element;
+	}
+
+	public static void Render(GameObject host, Element<object> element)
+	{
+		BirbRoot root = host.AddComponent(typeof(BirbRoot)) as BirbRoot;
+		root.element = element;
+	}
+}
